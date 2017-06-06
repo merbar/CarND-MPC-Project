030582cdfe61b0a8cf88dde947bdd9882c2196ca
Marcus Erbar <marcus.erbar@gmail.com>
2017-06-06 10:33:54 -0700

    optimization pass

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -145,7 +145,7 @@ class FG_eval {
 MPC::MPC() {}
 MPC::~MPC() {}
 //=============================================================================
-vector<double> MPC::Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs) {
+vector<double> MPC::Solve(const Eigen::VectorXd &state, const Eigen::VectorXd &coeffs) {
   bool ok = true;
   size_t i;
   typedef CPPAD_TESTVECTOR(double) Dvector;
--- src/MPC.h
+++ src/MPC.h
@@ -15,7 +15,7 @@ class MPC {
 
   // Solve the model given an initial state and polynomial coefficients.
   // Return the first actuations.
-  vector<double> Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs);
+  vector<double> Solve(const Eigen::VectorXd &state, const Eigen::VectorXd &coeffs);
 };
 
 #endif /* MPC_H */
